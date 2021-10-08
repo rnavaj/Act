55832ff3d793dcb6d14c614a73938b2d16b99729
rnavaj <navwaaz@gmail.com>
2021-10-08 11:31:36 +0530

    success

---
+++ src/org/scan/Canner.java
@@ -0,0 +1,46 @@
+package org.scan;
+
+import java.util.Scanner;
+
+public class Canner {
+	public static void main(String[] args) {
+		
+		Scanner d =new Scanner(System.in);
+	     System.out.println("college");
+	      String t =d.nextLine();
+	      System.out.println(t);
+	      
+		System.out.println("student name");
+		  String r= d.next();
+		 System.out.println(r);
+		 
+		System.out.println("Department name");
+		  String i=d.next();
+		  System.out.println(i);
+		   int id=d.nextInt();
+		   System.out.println(id);
+
+		   System.out.println("contact numbers");
+		   int is=d.nextInt();
+		   System.out.println(is);
+		   
+		System.out.println("Email.id");
+		  String o =d.next();
+		  System.out.println(o);
+		
+		  System.out.println("gender");
+		  String lr=d.next();
+		  System.out.println(lr);
+		  
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
